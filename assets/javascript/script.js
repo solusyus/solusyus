2321442dfebe92d778cd3ebdec9d715e4036072e
solusyus <solusyus+github@gmail.com>
2025-09-18 23:02:12 +0530

    Update script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,27 @@
+// Sooiler Tag
+let spoil = document.getElementsByClassName("spoiler");
 
+for (let i = 0; i < spoil.length; i++) {
+  spoil[i].onclick = function () {
+    if (this.classList.contains("spoiler")) {
+      this.style.color = "--spoilerBGColor";
+      this.style.backgroundColor = "--spoiled";
+      this.classList.remove("spoiler");
+      this.classList.add("spoiled");
+    } else if (this.classList.contains("spoiled")) {
+      this.style.color = "";
+      this.style.backgroundColor = "";
+      this.classList.remove("spoiled");
+      this.classList.add("spoiler");
+    }
+  };
+}
+
+  // Scroll-button
+document.getElementById("scrollTop").onclick = () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
+
+document.getElementById("scrollBottom").onclick = () => {
+  window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+};
